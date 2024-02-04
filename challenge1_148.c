77b38ae6b3c40f50ac21974ffab776ac9a0ac99a
kar1f <145224790+kar1f@users.noreply.github.com>
2024-02-04 17:42:51 +0530

    Create challenge1_148.c

---
+++ challenge1_148.c
@@ -0,0 +1,18 @@
+// Author:
+// Author URL: 
+
+#include "stdio.h"
+
+int main(){
+    int principal;
+    scanf("%d\n", &principal);
+    int rate_of_interest;
+    scanf("%d\n", &rate_of_interest);
+    int time;
+    scanf("%d\n", &time);
+    double simple_interest;
+    simple_interest = (principal * time * rate_of_interest) / 100;
+    printf("%0.1lf", simple_interest);
+
+    return 0;
+}
